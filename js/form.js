4c5f9067e4dde449fb8212a7f777202ab3648f97
Anna <a.s.maluygina@gmail.com>
2024-05-29 21:51:14 +0300

    feat: add btn disabled

--- js/form.js
+++ js/form.js
@@ -1,10 +1,26 @@
-const form = document.querySelector('.form');
-form.addEventListener('submit', handleFormSubmit);
+const form = document.querySelector(".form");
+const submitBtn = document.getElementById("btn-submit");
+
+form.addEventListener("submit", handleFormSubmit);
+form.addEventListener("input", validateForm);
 
 function handleFormSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
+  if (form.checkValidity()) {
+    window.location.href = "confirmation.html";
+  }
+}
 
-    if (form.checkValidity()) {
-        window.location.href = 'confirmation.html'
-    }
+function validateForm() {
+  const formFields = form.querySelectorAll("input, select");
+  const isFormValid = Array.from(formFields).every(
+    (field) => field.value.trim() !== ""
+  );
+  if (isFormValid) {
+    submitBtn.disabled = false;
+    submitBtn.classList.remove("disabled");
+  } else {
+    submitBtn.disabled = true;
+    submitBtn.classList.add("disabled");
+  }
 }
